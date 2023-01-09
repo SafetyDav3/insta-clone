6a525fb66f1ac7e4cdbd0b90f33228c4a3d271ee
SafteyDav3 <talktomedw@gmail.com>
2023-01-09 15:35:22 -0600

    Header completed

--- components/Header.js
+++ components/Header.js
@@ -10,14 +10,14 @@ import {
   UserGroupIcon,
   HeartIcon,
   PaperAirplaneIcon,
-  Bars4Icon,
+  Bars3Icon,
 } from "@heroicons/react/24/outline";
 
 import { HomeIcon } from "@heroicons/react/24/solid";
 
 function Header() {
   return (
-    <div>
+    <div className="shadow-sm border-b bg-wheat sticky top-0 z-50">
       <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
         {/* left */}
         <div className="relative hidden lg:inline-grid w-24">
@@ -46,11 +46,23 @@ function Header() {
         {/* right */}
         <div className="flex items-center justify-end space-x-4 ">
           <HomeIcon className="navBtn" />
-          <PaperAirplaneIcon className="navBtn" />
+          <div className="relative navBtn">
+            <PaperAirplaneIcon className="navBtn -rotate-45" />
+            <div className="absolute bg-red-500 -top-1 -right-3 text-xs w-5 h-5 rounded-full flex items-center justify-center animate-pulse text-white">
+              3
+            </div>
+          </div>
           <PlusCircleIcon className="navBtn" />
           <UserGroupIcon className="navBtn" />
           <HeartIcon className="navBtn" />
-          <Bars4Icon className="h-6 w-10 text-black md:hidden cursor-pointer hover:scale-125 transition-all duration-150 ease-out" />
+
+          <img
+            src="https://www.caresource.com/wp-content/uploads/2021/11/David-Williams-2021-300x300-2.png"
+            alt="David Williams"
+            className="h-10 rounded-full cursor-pointer hover:scale-125 transition-all duration-150 ease-out"
+          />
+
+          <Bars3Icon className="h-6 w-10 text-black md:hidden cursor-pointer hover:scale-125 transition-all duration-150 ease-out" />
         </div>
       </div>
     </div>
