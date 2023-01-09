0d117babbf16fce5c5bdbc76df66363486c2efeb
SafteyDav3 <talktomedw@gmail.com>
2023-01-09 13:34:03 -0600

    menu and nav buttons added and reactive

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
-import instaCon from "../public/icons/icons8-instagram.svg"
-import instaWord from "../public/icons/ocw.webp"
+import instaCon from "../public/icons/icons8-instagram.svg";
+import instaWord from "../public/icons/ocw.webp";
 
 // Icon imports
 import {
@@ -11,44 +11,50 @@ import {
   HeartIcon,
   PaperAirplaneIcon,
   Bars4Icon,
-} from "@heroicons/react/24/solid"
+} from "@heroicons/react/24/outline";
+
+import { HomeIcon } from "@heroicons/react/24/solid";
 
 function Header() {
   return (
     <div>
-      <div className="flex justify-between max-w-6xl">
+      <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
         {/* left */}
         <div className="relative hidden lg:inline-grid w-24">
-          <Image
-            src={instaWord}
-            layout="fill"
-            objectFit="contain"
-          />
+          <Image src={instaWord} layout="fill" objectFit="contain" />
         </div>
 
         <div className="w-10 lg:hidden relative cursor-pointer">
-          <Image
-            src={instaCon}
-            layout="fill"
-            objectFit="contain"
-          />
+          <Image src={instaCon} layout="fill" objectFit="contain" />
         </div>
 
         {/* Middle */}
 
-        <div className="relative mt-1 p-3 rounded-md">
-          <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
-            {/* Deprecated and needs to use version-2 */}
-            <MagnifyingGlassIcon className='h-5 w-5 text-gray-500'  />
+        <div className="max-w-xs">
+          <div className="relative mt-1 p-3 rounded-md">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <MagnifyingGlassIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              className="bg-gray-50 block w-full pl-10 rounded-md sm:text-sm border-gray-300 focus:ring-black focus:border-black"
+              type="text"
+              placeholder="Search"
+            />
           </div>
-          <input className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 rounded-md focus:ring-black focus:border-black" type="text" placeholder="Search" />
         </div>
 
         {/* right */}
+        <div className="flex items-center justify-end space-x-4 ">
+          <HomeIcon className="navBtn" />
+          <PaperAirplaneIcon className="navBtn" />
+          <PlusCircleIcon className="navBtn" />
+          <UserGroupIcon className="navBtn" />
+          <HeartIcon className="navBtn" />
+          <Bars4Icon className="h-6 w-10 text-black md:hidden cursor-pointer hover:scale-125 transition-all duration-150 ease-out" />
+        </div>
       </div>
     </div>
   );
 }
 
 export default Header;
-
