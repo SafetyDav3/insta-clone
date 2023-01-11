18abaf805f6501b5017e3faafad842416b4731d5
SafteyDav3 <talktomedw@gmail.com>
2023-01-10 18:45:29 -0600

    faker-js is working to create arry

--- components/Stories.js
+++ components/Stories.js
@@ -1,18 +1,25 @@
-import React from 'react'
+// Generate fake users with Faker.js --- Solution found here: https://stackoverflow.com/questions/73006706/is-there-a-new-form-of-the-contextualcard-feature-in-faker-js/73661832#73661832
 
-function Stories() {
+import { faker } from "@faker-js/faker";
+import { useState, useEffect } from "react";
 
-//  stuck here!!!!!!!!!!!!!!!!!!!!
+export default function Stories() {
+  const [suggestions, setSuggestions] = useState([]);
+  useEffect(() => {
+    const suggestions = [...Array(20)].map((_, i) => ({
+      userId: faker.datatype.uuid(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      avatar: faker.image.avatar(),
+      password: faker.internet.password(),
+      birthdate: faker.date.birthdate(),
+      registeredAt: faker.date.past(),
+    }));
+    setSuggestions(suggestions);
+  }, []);
+
+  
 
 
 
-
-
-    return (
-        <div>
-
-        </div>
-  )
 }
-
-export default Stories
